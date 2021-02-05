bd8ad8d7dee155a7fe1587ea48f034e2cd2bd998
Shohag <shohagrana@appscode.com>
2021-02-05 16:54:54 +0600

    edited 14forloop.go

--- 14forloop.go
+++ 14forloop.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	//Go has only one loop =====> the for loop
+	//while loop using for loop
 	i := 1
 	for i <= 10 {
 		fmt.Println(i)
 		i = i + 1
+	}
+	//for loop
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
 	}
 }
