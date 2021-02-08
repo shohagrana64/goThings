9672197b4d3c8538617f807c834d54c58f68724c
Shohag <shohagrana@appscode.com>
2021-02-08 15:23:30 +0600

    edit 33

--- gobookPractice/32defer.go
+++ gobookPractice/32defer.go
@@ -10,7 +10,7 @@ func second() {
 }
 func main() {
 	//defer runs the method at the end of main() or wherever it is called.
-
+	//defer always executes, even if there is a runtime error.
 	defer second()
 	first()
 	fmt.Println("Hi")
