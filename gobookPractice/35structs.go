5a6fde9fc87502ad5a1a4cf8231260b897435347
Shohag <shohagrana@appscode.com>
2021-02-08 18:23:02 +0600

    edit 35

--- gobookPractice/35structs.go
+++ gobookPractice/35structs.go
@@ -23,6 +23,22 @@ type Circle struct {
 func circleArea(c Circle) float64 {
 	return math.Pi * c.r * c.r
 }
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y1 - y2
+	return math.Sqrt(a*a + b*b)
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+	return r.x1
+}
 func main() {
 	//initialization:
 	//
@@ -39,4 +55,8 @@ func main() {
 	fmt.Println(c.x, c.y, c.r)
 	fmt.Println(circleArea(c))
 	fmt.Println(c)
+
+	fmt.Println("Now rectangle:")
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(r.area())
 }
