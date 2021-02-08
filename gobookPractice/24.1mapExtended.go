fe2688dd390be7f113fc83da007418686c79dfea
Shohag <shohagrana@appscode.com>
2021-02-08 12:34:39 +0600

    edit 24.1

---
+++ gobookPractice/24.1mapExtended.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//map creation
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["Li"])
+}
