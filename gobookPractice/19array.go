bd404c711fe107c14a8cc1a6631912c072be7683
Shohag <shohagrana@appscode.com>
2021-02-08 11:29:43 +0600

    edit 19

--- gobookPractice/19array.go
+++ gobookPractice/19array.go
@@ -6,4 +6,18 @@ func main() {
 	var x [5]int
 	x[4] = 100
 	fmt.Println(x)
+
+	//another way to initialize array
+	y := [5]float64{98, 93, 77, 82, 83}
+	fmt.Println(y)
+
+	//another way to initialize array in multiple lines
+	z := [5]float64{
+		98,
+		93,
+		77,
+		82,
+		83, //extra comma
+	}
+	fmt.Println(z)
 }
