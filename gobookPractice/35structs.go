cced6c79401ac08cc2c5bf41881db62a4b8eeaf5
Shohag <shohagrana@appscode.com>
2021-02-08 16:53:21 +0600

    edit 35

---
+++ gobookPractice/35structs.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/*
+define a Circle struct:
+
+type Circle struct {
+	x float64
+	y float64
+	r float64
+} */
+
+//define values of same type:
+
+type Circle struct {
+	x, y, r float64
+}
+
+func main() {
+	//initialization:
+	//
+	//1) var c Circle
+	//2) c := new(Circle)
+	//
+	//	initialization with variables:
+	//3) c := Circle{x: 0, y: 0, r: 5}
+	//if order is known:
+	c := Circle{0, 0, 5}
+	fmt.Println(c.x, c.y, c.r)
+	c.x = 10
+	c.y = 5
+	fmt.Println(c.x, c.y, c.r)
+}
