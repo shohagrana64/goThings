f6a2b3b7d3dea995b2a532ca74a66fda7ca24208
Shohag <shohagrana@appscode.com>
2021-02-09 15:27:55 +0600

    edit 38.3

---
+++ gobookPractice/38.3channels.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func calcSquares(number int, squareop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit
+		number /= 10
+	}
+	squareop <- sum
+}
+
+func calcCubes(number int, cubeop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit * digit
+		number /= 10
+	}
+	cubeop <- sum
+}
+
+/*
+This program will print the sum of the squares and cubes of the individual digits of a number.
+For example if 123 is the input, then this program will calculate the output as:
+
+squares = (1 * 1) + (2 * 2) + (3 * 3)
+cubes = (1 * 1 * 1) + (2 * 2 * 2) + (3 * 3 * 3)
+output = squares + cubes = 50
+*/
+func main() {
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
+	go calcSquares(number, sqrch)
+	go calcCubes(number, cubech)
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final output", squares+cubes)
+}
