3c72f06854ee60e172162fa23c450edb93b3a1fe
Shohag <shohagrana@appscode.com>
2021-02-10 12:55:42 +0600

    edit 49

---
+++ gobookPractice/49mutex.go
@@ -0,0 +1,25 @@
+package main
+
+//Concurrency better than MUTEX. MUTEX is not used in go . Instead concurrency is used.
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	m := new(sync.Mutex)
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			m.Lock()
+			fmt.Println(i, "start")
+			time.Sleep(time.Second)
+			fmt.Println(i, "end")
+			m.Unlock()
+		}(i)
+	}
+
+	var input string
+	fmt.Scanln(&input)
+}
