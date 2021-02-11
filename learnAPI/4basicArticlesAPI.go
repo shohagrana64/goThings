6e7ffd6d775d37300b62983979d8f7faded247c2
Shohag <shohagrana@appscode.com>
2021-02-11 11:44:39 +0600

    edit 4

---
+++ learnAPI/4basicArticlesAPI.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test title1", Desc: "Test Description1", Content: "Shabalaba DingDong"},
+		Article{Title: "Test title2", Desc: "Test Description2", Content: "Giggity Giggity"},
+	}
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+func main() {
+	handleRequests()
+}
