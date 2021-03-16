d513ce712a8e60b3de3ae630154c1bc319e5f0e8
shohag <shohagrana@appscode.com>
2021-03-16 10:01:47 +0600

    edit example

--- Client_GO/deployment.go
+++ Client_GO/deployment.go
@@ -128,68 +128,19 @@ func deleteDeployment(args []string) {
 
 func main() {
 	clientset = createClientSet()
-	createDeployment()
-	getDeployments()
-	deleteDeployment([]string{"bookapi-deployment-go"})
-	// Update Deployment
+	fmt.Println("Press enter to Create bookapi-deployment-go")
 	prompt()
-	fmt.Println("Updating deployment...")
-	//    You have two options to Update() this Deployment:
-	//
-	//    1. Modify the "deployment" variable and call: Update(deployment).
-	//       This works like the "kubectl replace" command and it overwrites/loses changes
-	//       made by other clients between you Create() and Update() the object.
-	//    2. Modify the "result" returned by Get() and retry Update(result) until
-	//       you no longer get a conflict error. This way, you can preserve changes made
-	//       by other clients between Create() and Update(). This is implemented below
-	//			 using the retry utility package included with client-go. (RECOMMENDED)
-	//
-	// More Info:
-	// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
+	createDeployment()
+	fmt.Println("Press enter to see the deployments")
+	prompt()
+	getDeployments()
+	fmt.Println("Press enter to delete the bookapi-deployment-go")
+	prompt()
+	deleteDeployment([]string{"bookapi-deployment-go"})
 
-	//retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-	//	// Retrieve the latest version of Deployment before attempting update
-	//	// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-	//	result, getErr := deploymentsClient.Get(context.TODO(), "bookapi-deployment-go", metav1.GetOptions{})
-	//	if getErr != nil {
-	//		panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
-	//	}
-	//
-	//	result.Spec.Replicas = int32Ptr(1)                           // reduce replica count
-	//	result.Spec.Template.Spec.Containers[0].Image = "nginx:1.13" // change nginx version
-	//	_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
-	//	return updateErr
-	//})
-	//if retryErr != nil {
-	//	panic(fmt.Errorf("Update failed: %v", retryErr))
-	//}
-	//fmt.Println("Updated deployment...")
-	//
-	//// List Deployments
-	//prompt()
-	//fmt.Printf("Listing deployments in namespace %q:\n", apiv1.NamespaceDefault)
-	//list, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
-	//if err != nil {
-	//	panic(err)
-	//}
-	//for _, d := range list.Items {
-	//	fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
-	//}
-	//
-	//// Delete Deployment
-	//prompt()
-	//fmt.Println("Deleting deployment...")
-	//deletePolicy := metav1.DeletePropagationForeground
-	//if err := deploymentsClient.Delete(context.TODO(), "demo-deployment", metav1.DeleteOptions{
-	//	PropagationPolicy: &deletePolicy,
-	//}); err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("Deleted deployment.")
 }
 
 func prompt() {
-	fmt.Printf("-> Press Return key to continue.")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		break
